f422f54f7a79d7d590694d3ff11a05ede31c7dc3
aoik109 <allison.ko.family@gmail.com>
2024-03-02 16:13:10 -0800

    add player and winner display

--- src/App.js
+++ src/App.js
@@ -26,7 +26,17 @@ export default function Board() {
     setXIsNext(!xIsNext)
   }
 
+  // keeping track of winner or next player
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = `Winner: ${winner}`
+  } else {
+    status = `Next Player: ${xIsNext ? 'X' : 'O'}`
+  }
+
   return (<>
+    <div className="status">{status}</div>
     <div className="board-row">
       <Square value={squares[0]} onSquareClick={() => {handleClick(0)}}/>
       <Square value={squares[1]} onSquareClick={() => {handleClick(1)}}/>
